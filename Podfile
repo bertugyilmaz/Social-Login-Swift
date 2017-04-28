161c1a6c88a19e660eea67c1b107bcbc1554f3b5
Bertuğ <ylmazbertug@gmail.com>
2017-04-28 10:32:05 +0300

    Added SDWebImage

--- Podfile
+++ Podfile
@@ -7,5 +7,6 @@ target 'SocialLogin' do	use_frameworks!
 	pod 'SDWebImage', '~>3.8'
 	pod 'FacebookCore'
 	pod 'FacebookLogin'
+	pod 'FacebookShare' 
 
 end
--- SocialLogin/LoginViewController.swift
+++ SocialLogin/LoginViewController.swift
@@ -7,15 +7,33 @@
 //
 
 import UIKit
+import FBSDKCoreKit
 import FBSDKLoginKit
+import FBSDKShareKit
 import Alamofire
 import SDWebImage
 
 class LoginViewController: UIViewController, FBSDKLoginButtonDelegate {
+    
+    let imageURL = "https://img.clipartfest.com/b68ba2ac07e002167057770f502dca3b_facebook-icon-vector-facebook-logo_512-512.png"
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var UsernameTextField: UITextField!
+    @IBOutlet weak var PasswordTextField: UITextField!
+    
+    @IBOutlet weak var FacebookButton: FBSDKLoginButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-      
+        if(FBSDKAccessToken.current() == nil){
+            print("Not logged in ")
+        }else{
+            print("Logged in already")
+        }
+        
+        FacebookButton.readPermissions = ["public_profile","email"]
+        FacebookButton.delegate = self
+        
+    imageView.sd_setImage(with: URL(string: imageURL), placeholderImage: UIImage(named: "placeholder.png"))
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,14 +43,17 @@ class LoginViewController: UIViewController, FBSDKLoginButtonDelegate {
     func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!){
         
         if((error) != nil){
-            print("Bir Hata Var")
+            print("Something going is wrong?")
         }else{
-            print("Giriş Yapıldı!")
-        }
+            print("Log-in")
+            print("-----------------------------------")
+            //FBSDKGraphRequest.init(graphPath: "/me", parameters: )
+       }
+        
     }
     
     func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!){
-        print("Logout ..")
+        print("Log-out")
     }
 
 
