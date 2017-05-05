4e002b914fb66edf99d2a5235f2cb10534264d1d
Bertuğ <ylmazbertug@gmail.com>
2017-05-05 13:39:54 +0300

    Last Changes

--- SocialLogin/AppDelegate.swift
+++ SocialLogin/AppDelegate.swift
@@ -18,11 +18,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
         FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
-        
         return true
     }
     
     func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
+        
+        if LISDKCallbackHandler.shouldHandle(url) {
+            return LISDKCallbackHandler.application(application,open:url,sourceApplication:sourceApplication,annotation:annotation)
+        }
         return FBSDKApplicationDelegate.sharedInstance().application(application, open: url, sourceApplication: sourceApplication, annotation: annotation)
     }
 
--- SocialLogin/LoginViewController.swift
+++ SocialLogin/LoginViewController.swift
@@ -8,43 +8,62 @@
 
 import UIKit
 import FBSDKLoginKit
-//import Alamofire
 import SDWebImage
 
 class LoginViewController: UIViewController, FBSDKLoginButtonDelegate {
     
     let imageURL = "https://img.clipartfest.com/b68ba2ac07e002167057770f502dca3b_facebook-icon-vector-facebook-logo_512-512.png"
 
+    @IBOutlet weak var linkedButton : UIButton!
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var UsernameTextField: UITextField!
     @IBOutlet weak var PasswordTextField: UITextField!
-    
     @IBOutlet weak var FacebookButton: FBSDKLoginButton!
+    var AccessToken: LISDKAccessToken!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.linkedButton.addTarget(self, action: #selector(linkedinLoginAction), for: .touchUpInside)
+        
         if(FBSDKAccessToken.current() == nil){
-            print("---------------------------------------------------------------------------------------------------------")
             print("Not logged in ")
-            print("---------------------------------------------------------------------------------------------------------")
         }else{
-            print("---------------------------------------------------------------------------------------------------------")
             print("Logged in already")
-            print("---------------------------------------------------------------------------------------------------------")
             getFacebookUserInfo()
         }
         
         FacebookButton.readPermissions = ["public_profile","email"]
         FacebookButton.delegate = self
         
-    imageView.sd_setImage(with: URL(string: imageURL), placeholderImage: UIImage(named: "placeholder.png"))
+        imageView.sd_setImage(with: URL(string: imageURL), placeholderImage: UIImage(named: "placeholder.png"))
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     
+    
+    func linkedinLoginAction(sender:UIButton){
+        LISDKSessionManager.createSession(withAuth: [LISDK_BASIC_PROFILE_PERMISSION], state: "", showGoToAppStoreDialog: true, successBlock: { (response : String?) in
+            let url = "https://api.linkedin.com/v1/people/~?format=json"
+            guard let session = LISDKSessionManager.sharedInstance().session else{
+                return
+            }
+            self.AccessToken = session.accessToken
+            print("Token --> \(self.AccessToken)")
+            
+            LISDKAPIHelper.sharedInstance().getRequest(url, success: { (response) in
+                print(response?.headers)
+            }, error: { (error) in
+                print(error)
+            })
+        }) { (error:Error?) in
+            print(error as Any)
+        }
+    }
+    
     func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!){
-        
         if((error) != nil){
             print("Something going is wrong?")
         }else{
@@ -60,14 +79,12 @@ class LoginViewController: UIViewController, FBSDKLoginButtonDelegate {
     func getFacebookUserInfo(){
         let params = ["fields" : "email, name"]
         let graphRequest = FBSDKGraphRequest.init(graphPath: "/me", parameters: params)
-        
         let Connection = FBSDKGraphRequestConnection()
         
         Connection.add(graphRequest) { (Connection, result, error) in
             let info = result as! [String : AnyObject]
-            print("---------------------------------------------------------------------------------------------------------")
+            
             print(info)
-            print("---------------------------------------------------------------------------------------------------------")
         }
         Connection.start()
     }
